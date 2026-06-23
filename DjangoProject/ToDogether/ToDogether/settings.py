2f7508bcb60b12c52f02c9f82f0dfca9e3218664
tiyachaudhari <tiyac48@gmail.com>
2026-06-23 22:17:14 +0100

    Initial commit

--- DjangoProject/ToDogether/ToDogether/settings.py
+++ DjangoProject/ToDogether/ToDogether/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-306nea302!1b62vd=afxrvl!w^dj67)-w2ag8xa-y%uq=nw78s'
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -149,5 +150,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = 'mariposaco.el@gmail.com' 
-EMAIL_HOST_PASSWORD = 'gyuq jbzy zfle nvsb'  
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
